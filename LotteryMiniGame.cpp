4f3e8cc8ec562b22a47338422f5fb1e37283a10f
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-10-07 15:28:14 +0800

    fix the problem of ERROR when user input point

--- LotteryMiniGame.cpp
+++ LotteryMiniGame.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
 	int money=0,lottery=0; 	// 金錢與樂透數初值為0
-	int choose;		// 選項
+	char choose;		// 選項
 	srand(time(NULL));
 	while(true){
 		int lottery_num=0,lottery_result=0;	// 樂透號碼
@@ -20,13 +20,16 @@ int main(){
 		cout<<"4) 離開"<<endl<<endl;
 		// 輸入
 		cin>>choose;
+		if(!cin){
+			cout<<"輸入錯誤"<<endl; 
+		}
 		// 判斷
 		switch(choose){
-			case 1:	// 儲值功能
+			case '1':	// 儲值功能
 				money=money+100;
 				cout<<"加值完成"<<endl<<endl;
 				break;
-			case 2:	// 下注功能
+			case '2':	// 下注功能
 				if(money>=50){
 					money=money-50;
 					lottery=lottery+1;
@@ -37,7 +40,7 @@ int main(){
 					cout<<"你的錢不夠下注了"<<endl<<endl;
 				}
 				break;
-			case 3: // 兌獎
+			case '3': // 兌獎
 				if(lottery<1){
 					cout<<"你現在沒有樂透"<<endl<<endl;
 				}else{
@@ -51,10 +54,11 @@ int main(){
 					}
 				}	
 				break;
-			case 4: // 退出程式
+			case '4': // 退出程式
 				cout<<"謝謝光臨"<<endl<<endl;
 				return 0;
 			default:
+				cout<<"輸入錯誤"<<endl<<endl;
 				break;
 		}
 	}
